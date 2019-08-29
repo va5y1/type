0bc9db0c22ce2b7a849142ecb6401f5b02c534c9
va5y1 <pro100rad@gmail.com>
2019-08-29 22:34:01 +0300

    init commit

---
+++ starterpack/gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+var notify = require("gulp-notify");
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', async function(){
+  gulp.src(['scss/*.scss'])
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: true
+      }))
+      .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream())
+   .pipe(notify(" Done! \n Your CSS files are ready, Master."))
+});
+
+gulp.task('sass:watch', function(){
+  gulp.watch(['scss/*.scss'], gulp.series('sass'));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init(["style/*.css", "*.html"], {
+    server: {
+      baseDir: "./"
+    }
+  })
+});
+
+gulp.task('watch', gulp.series('sass', gulp.parallel('sass:watch', 'browser-sync')));
+
+gulp.task('default', gulp.series('watch'));
